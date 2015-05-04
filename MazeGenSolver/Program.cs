89a7e74f5ecaa888af7644ac9f09e41adf611480
Lackster <jproulx2@IT100.mr.preble.com>
2015-05-04 13:17:31 -0400

    Derp1
    Iderpedit

--- MazeGenSolver/Program.cs
+++ MazeGenSolver/Program.cs
@@ -3,18 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace MazeGenSolver
 {
     class Program
     {
-        [STAThread]
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
         }
     }
+
 }
